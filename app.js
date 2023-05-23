c12f83088a88b05696e0abf5ad604e3f52c6f4dc
MuneimAsif <127970035+MuneimAsif@users.noreply.github.com>
2023-05-23 11:07:51 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var inp1 = document.getElementById("inp1");
+var inp2 = document.getElementById("inp2")
+var inp3 = document.getElementById("inp3")
+var inp4 = document.getElementById("inp4")
+var inp5 = document.getElementById("inp5")
+var select1 = document.getElementById("slct1")
+var select2 = document.getElementById("slct2")
+var select3 = document.getElementById("slct3")
+function register() {
+    alert(inp1.value)
+    alert(inp2.value)
+    alert(inp3.value)
+    alert(inp4.value)
+    alert(select1.value)
+    alert(select2.value)
+    alert(select3.value)
+    alert(inp5.value)
+}
